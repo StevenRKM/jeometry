97ec7af9d5dee0e53ccec7a706512c26cdf87077
Steven Reekmans <steven.rkm@gmail.com>
2013-03-24 22:11:52 +0100

    easier namespacing, distances

--- jeometry-distance.js
+++ jeometry-distance.js
@@ -1,16 +1,19 @@
 (function( jeometry, undefined ) {
 
-    // sub-namespace
-    jeometry.distance = {};
-
     // TODO should accept any primitive combination
-    jeometry.distance.euclidean = function(p1, p2) {
+    var euclidean = function(p1, p2) {
         return Math.sqrt( Math.pow(p1.x-p2.x, 2) + Math.pow(p1.y-p2.y, 2) );
     };
 
-    // TODO should accept any primitive combinationfunction distance(p1, p2) {
-    jeometry.distance.manhattan = function(p1, p2) {
+    // TODO should accept any primitive combination
+    var manhattan = function(p1, p2) {
         return Math.abs( p1.x-p2.x ) + Math.abs( p1.y-p2.y );
     };
+
+    // tie to namespace
+    jeometry.distance = {
+        euclidean:euclidean,
+        manhattan:manhattan
+    }
 
 }( window.jeometry = window.jeometry || {} ));
