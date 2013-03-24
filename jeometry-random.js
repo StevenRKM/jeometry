a513fd389c2240551df2ce07b47271d3418edc59
Steven Reekmans <steven.rkm@gmail.com>
2013-03-24 22:07:26 +0100

    easier namespacing, random

--- jeometry-random.js
+++ jeometry-random.js
@@ -1,16 +1,19 @@
 (function( jeometry, undefined ) {
 
-    // sub-namespace
-    jeometry.random = {};
-
-    jeometry.random.int = function(min, max) {
+    var int = function(min, max) {
         // [min, max] ==> min <= x <= max
         return Math.floor((Math.random()*(max-min+1))+min);
     };
 
-    jeometry.random.pointInBox = function(x, y, width, height) {
+    var pointInBox = function(x, y, width, height) {
         // [min, max] ==> min <= x <= max
-        return point( jeometry.random.int(x, x+width), jeometry.random.int(y, y+height) );
+        return point(int(x, x+width), int(y, y+height));
+    };
+
+    // tie to namespace
+    jeometry.random = {
+        int:int,
+        pointInBox:pointInBox
     };
 
 }( window.jeometry = window.jeometry || {} ));
