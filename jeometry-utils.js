d1b2a321b455ba9f8d5646b0301091e6382c474a
Steven Reekmans <steven.rkm@gmail.com>
2013-03-25 01:02:02 +0100

    added 2d lookup also used try catch on symmitrical 2d lookup

--- jeometry-utils.js
+++ jeometry-utils.js
@@ -16,30 +16,27 @@
         return (p1.x * p2.y) - (p1.y * p2.x);
     };
 
-    var create_2d_symmetric_lookup = function(data) {
+    var create_2d_lookup = function(data) {
         /*
-        Creates a lookup table where the key is made up of two elements.
-        These key elements can be given in any order, so the lookup is built symmetrical.
-            lookup.get(key1, key2) == lookup.get(key2, key1)
+         Creates a lookup table where the key is made up of two elements.
+         The order of these key elements is important.
+         lookup.get(key1, key2) != lookup.get(key2, key1)
 
-        data must be an array of elements like {key1, key2, value}
+         data must be an array of elements like {key1, key2, value}
 
-        example:
-        data = [{key1:"funky", key2:"town", value:"yeah baby!"}, ...]
-        lookup.get("funky", "town")
-        lookup.get("town", "funky")
-        */
+         example:
+         data = [{key1:"funky", key2:"town", value:"yeah baby!"}, ...]
+         lookup.get("funky", "town")
+         */
 
         var lookup = {
             data: {},
             get: function(key1, key2) {
-                if(this.data[key1] != undefined) {
-                    if(this.data[key1][key2] != undefined)
-                        return this.data[key1][key2];
-                } else if(this.data[key2] != undefined) {
-                    return this.data[key2][key1];
+                try {
+                    return this.data[key1][key2];
+                } catch(e) {
+                    return undefined;
                 }
-                return undefined
             }
         };
 
@@ -53,12 +50,52 @@
 
     };
 
-    // tie to sub-namespace
+    var create_2d_symmetric_lookup = function(data) {
+        /*
+         Creates a lookup table where the key is made up of two elements.
+         These key elements can be given in any order, so the lookup is built symmetrical.
+         lookup.get(key1, key2) == lookup.get(key2, key1)
+
+         data must be an array of elements like {key1, key2, value}
+
+         example:
+         data = [{key1:"funky", key2:"town", value:"yeah baby!"}, ...]
+         lookup.get("funky", "town")
+         lookup.get("town", "funky")
+         */
+
+        var lookup = {
+            data: {},
+            get: function(key1, key2) {
+                try {
+                    return this.data[key1][key2];
+                } catch(e) {
+                    try {
+                        return this.data[key2][key1];
+                    } catch(e) {
+                        return undefined;
+                    }
+                }
+            }
+        };
+
+        for(var i in data) {
+            if(lookup.data[data[i].key1] == undefined)
+                lookup.data[data[i].key1] = {};
+            lookup.data[data[i].key1][data[i].key2] = data[i].value;
+        }
+
+        return lookup;
+
+    };
+
+    // tie to namespace
     jeometry.utils = {
         point_add:point_add,
         point_subtract:point_subtract,
         point_scalar:point_scalar,
         cross_product:cross_product,
+        create_2d_lookup:create_2d_lookup,
         create_2d_symmetric_lookup:create_2d_symmetric_lookup
     };
 
