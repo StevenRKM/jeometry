89a7194ba06b6152f33f8234974be2d7ca7f2b92
Steven Reekmans <steven.rkm@gmail.com>
2013-04-05 00:42:27 +0200

    improved reflections

--- jeometry-intersect.js
+++ jeometry-intersect.js
@@ -96,6 +96,100 @@
         return p.add(r.multiply_scalar(t));
     };
 
+    var segments_plus = function(segment1, segment2) {
+        /*
+         method using cross products
+
+         segments
+         p + tr, with t element of [0, 1]
+         q + us, with s element of [0, 1]
+
+         intersection
+         p + tr = q + us
+         (p + tr) x s = (q + us) x s
+         (p x s) + (tr x s) = q x s  // s x s = 0
+         t (r x s) = q x s - p x s
+         t (r x s) = (q - p) x s
+         t = (q - p) x s / (r x s)
+
+         thus
+         if r x s = 0 => parallel segments
+
+         */
+
+        // --- fast bounding box checks
+
+        // TODO could be used for intersect of two AABB's
+
+        // check x-axis
+
+        var d1 = segment1.p2.x - segment1.p1.x;
+        var d2 = segment2.p2.x - segment2.p1.x;
+
+        var upper1 = (d1 < 0) ? segment1.p1.x : segment1.p2.x;
+        var lower1 = (d1 < 0) ? segment1.p2.x : segment1.p1.x;
+        var upper2 = (d2 < 0) ? segment2.p1.x : segment2.p2.x;
+        var lower2 = (d2 < 0) ? segment2.p2.x : segment2.p1.x;
+
+        if(upper1 < lower2 || upper2 < lower1)
+            return undefined;
+
+        // check y-ayis
+
+        var d1 = segment1.p2.y - segment1.p1.y;
+        var d2 = segment2.p2.y - segment2.p1.y;
+
+        var upper1 = (d1 < 0) ? segment1.p1.y : segment1.p2.y;
+        var lower1 = (d1 < 0) ? segment1.p2.y : segment1.p1.y;
+        var upper2 = (d2 < 0) ? segment2.p1.y : segment2.p2.y;
+        var lower2 = (d2 < 0) ? segment2.p2.y : segment2.p1.y;
+
+        if(upper1 < lower2 || upper2 < lower1)
+            return undefined;
+
+        // --- cross product intersection
+
+        var p = segment1.p1;
+        var r = segment1.p2.subtract(segment1.p1);
+        var q = segment2.p1;
+        var s = segment2.p2.subtract(segment2.p1);
+
+        var r_x_s = r.cross_product(s);
+
+        // parallel
+        if(r_x_s == 0)
+            return undefined;
+
+        var q_p = q.subtract(p);
+        var q_p_x_s = q_p.cross_product(s);
+
+        if(q_p_x_s == 0)
+            return undefined;
+
+        var t = q_p_x_s / r_x_s;
+
+        if( t < 0 || t > 1)
+            return undefined;
+
+        var q_p_x_r = q_p.cross_product(r);
+
+        if(q_p_x_r == 0)
+            return undefined;
+
+        var u = q_p_x_r / r_x_s;
+
+        if( u < 0 || u > 1)
+            return undefined;
+
+        return {
+            point: p.add(r.multiply_scalar(t)),
+            segment1:segment1,
+            segment2:segment2,
+            segment1t:t,
+            segment2t:u
+        };
+    };
+
     var triangle_with_segment = function(triangle, segment) {
         /*
          easy algorithm, based upon the intersection of segments
@@ -132,6 +226,7 @@
         _functions:_functions,
         get:get,
         segments:segments,
+        segments_plus:segments_plus,
         triangle_with_segment:triangle_with_segment
     }
 
--- testbed/js/canvas-drawing.js
+++ testbed/js/canvas-drawing.js
@@ -162,3 +162,47 @@ function clear() {
     scene.splice(0, scene.length);
     context.clearRect(0,0,width,height);
 }
+
+function RGBAColor(r, g, b, alpha) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.alpha = alpha;
+}
+
+RGBAColor.prototype.setFromHSV = function(h, s, v, alpha) {
+    // h = [0,360], s = [0,1], v = [0,1]
+    h = h % 360;
+
+    if (h < 0) {
+        h += 360;
+    }
+
+    var c = v * s;
+    var h1 = h / 60;
+    var x = c * (1 - Math.abs(h1%2 - 1));
+    var r1, g1, b1;
+
+    switch (Math.floor(h1)) {
+        case 0: r1 = c; g1 = x; b1 = 0; break;
+        case 1: r1 = x; g1 = c; b1 = 0; break;
+        case 2: r1 = 0; g1 = c; b1 = x; break;
+        case 3: r1 = 0; g1 = x; b1 = c; break;
+        case 4: r1 = x; g1 = 0; b1 = c; break;
+        case 5: r1 = c; g1 = 0; b1 = x; break;
+    }
+
+    var m = v - c;
+
+    this.r = Math.floor((r1 + m) * 255);
+    this.g = Math.floor((g1 + m) * 255);
+    this.b = Math.floor((b1 + m) * 255);
+    this.alpha = alpha;
+}
+
+RGBAColor.prototype.setRGBA = function(r, g, b, alpha) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.alpha = alpha;
+}
